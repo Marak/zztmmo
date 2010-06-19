81a6db0c8a753310012a60161d8108de90aa167f
Marak Squires <marak.squires@gmail.com>
2010-06-18 23:56:20 -0400

    fixing buffer problem

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ fs.readFile('/root/nodejitsu/apps/zzt/zztmmo/settings.json', function(err, data)
         sys.puts('No settings.json found. Using default settings');
     }
     try {
-        settings = JSON.parse(data);
+        settings = JSON.parse(data.toString());
     } catch (e) {
         sys.puts('Error parsing settings.json');
         process.exit(1);
